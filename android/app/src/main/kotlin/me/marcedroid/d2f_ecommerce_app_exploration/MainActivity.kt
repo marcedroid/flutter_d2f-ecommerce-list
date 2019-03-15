dd5e73bd4c61c0a1a247f63e2b6973f7896ca89e
marcelo <marcelo.tk3@gmail.com>
2019-03-15 16:40:42 -0500

    Inicializando poryecto

---
+++ android/app/src/main/kotlin/me/marcedroid/d2f_ecommerce_app_exploration/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.marcedroid.d2f_ecommerce_app_exploration
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
